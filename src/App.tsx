00eef6223c06c34c5089804f2d14fc48322cc42f
Mortier Martin <34869851+martinmortier@users.noreply.github.com>
2021-09-25 11:57:32 +0200

    create notification

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import logoNotification from './relaxIcon.png'
 
 function App() {
+
+  const notification = () => {
+    return chrome.notifications.create({
+      title: 'Reminder',
+      message:'Test reminder',
+      iconUrl:logoNotification,
+      type:'basic'
+    })
+  }
+
+  useEffect(() => {
+    notification()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
