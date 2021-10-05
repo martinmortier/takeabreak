d7ae5034b8af871a54bd92ea7bfc37456cfd021d
Mortier Martin <34869851+martinmortier@users.noreply.github.com>
2021-10-05 23:37:52 +0200

    Sync data from background to App component

--- public/background.js
+++ public/background.js
@@ -3,18 +3,17 @@ chrome.storage.onChanged.addListener(() => {
   chrome.storage.sync.get(
     ["startTimerNotification", "time"],
     ({ startTimerNotification, time }) => {
+       console.log(startTimerNotification, "////", time);
+      clearInterval(interval);
       if (startTimerNotification && time > 0) {
-        clearInterval(interval)
-        interval = setInterval(
-          () => {
-            chrome.notifications.create({
-              title: "Reminder",
-              message: "Test reminder",
-              iconUrl: "./relaxIcon.png",
-              type: "basic",
-            })},
-          time
-        );
+        interval = setInterval(() => {
+          chrome.notifications.create({
+            title: "Reminder",
+            message: "Test reminder",
+            iconUrl: "./relaxIcon.png",
+            type: "basic",
+          });
+        }, time);
       }
     }
   );
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import { Switch } from "@mui/material";
-import React,{ MouseEventHandler, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import TimerForm from "./components/TimerForm";
 function App() {
-  const VALUE_START = 5000
-  const [time, setTime] = useState<number>(VALUE_START);
-  const [startTimerNotification, setStartTimerNotification] = useState<boolean>(false)
+  const VALUE_START = 5000;
+  const [time, setTime] = useState<number>(5000);
+  const [startTimerNotification, setStartTimerNotification] = useState<boolean>(false);
 
-  const startTimer = (time: number): void => {
-    setStartTimerNotification(true)
+  const startTimer = (time: number, checked: boolean): void => {
     chrome.storage.sync.set({
-      startTimerNotification: startTimerNotification,
+      startTimerNotification: checked,
       time: time,
     });
   };
@@ -18,19 +17,38 @@ function App() {
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>): void => {
     const number: number = Number(e.target.value);
     setTime(number);
-    startTimer(number)
+    startTimer(number, startTimerNotification);
   };
 
-  const handleChangeStartTimer = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    e.preventDefault()
-    setStartTimerNotification(e.target.checked)
-    startTimer(time)
+  const handleChangeStartTimer = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    e.preventDefault();
+    setStartTimerNotification(e.target.checked);
+    console.log("TIME",time);
+    startTimer(time, e.target.checked);
+  };
+  
+  const syncData = (): void => {
+    chrome.storage.sync.get(
+      ["startTimerNotification", "time"],
+      ({ startTimerNotification, time }) => {
+        setStartTimerNotification(startTimerNotification)
+        if(time) setTime(time)
+      }
+    );
   }
+
+  useEffect(() => {
+    syncData()
+  }, []);
 
   return (
     <div>
-      <Switch name="Start timer" onChange={handleChangeStartTimer}/>
-      <TimerForm handleChange={handleChange} time={time}/>
+      <Switch name="Start timer" onChange={handleChangeStartTimer} checked={startTimerNotification}/>
+      {startTimerNotification && (
+        <TimerForm handleChange={handleChange} time={time} />
+      )}
     </div>
   );
 }
