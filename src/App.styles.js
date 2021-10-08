2a0aab45a4d06037ab69b509fb2339b1a93f0a38
Mortier Martin <34869851+martinmortier@users.noreply.github.com>
2021-10-08 20:47:17 +0200

    Add styling App component file

---
+++ src/App.styles.js
@@ -0,0 +1,18 @@
+import { makeStyles } from "@mui/styles";
+
+export const useStyles = makeStyles({
+  root: {
+    background: "#84a59d",
+    display: "flex",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignContent: "center",
+    flexShrink: 1,
+  },
+  title: {
+    fontWeight: "bold",
+    color:"#f7ede2"
+  },
+});
+
