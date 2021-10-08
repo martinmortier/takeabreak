355fb2277250ba3286fe085261564b11d977fb0f
Mortier Martin <34869851+martinmortier@users.noreply.github.com>
2021-10-08 21:44:53 +0200

    Fix timer

--- src/components/TimerForm.tsx
+++ src/components/TimerForm.tsx
@@ -1,5 +1,10 @@
-import React, { ChangeEventHandler } from "react";
-import { FormControl, MenuItem, Select, SelectChangeEvent } from "@mui/material";
+import React from "react";
+import {
+  FormControl,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
 
 type TimerFormprops = {
   handleChange: (e: SelectChangeEvent<number>) => void;
@@ -9,13 +14,13 @@ const TimerForm = ({ handleChange, time }: TimerFormprops) => {
   return (
     <div>
       <FormControl fullWidth>
-      <Select value={time} onChange={handleChange}>
-        <MenuItem value={5000}>5 secondes</MenuItem>
-        <MenuItem value={500000}>5 minutes</MenuItem>
-        <MenuItem value={15000}>15 minutes</MenuItem>
-        <MenuItem value={300000}>30 minutes</MenuItem>
-        <MenuItem value={600000}>1 hours</MenuItem>
-      </Select>
+        <Select value={time} onChange={handleChange}>
+          <MenuItem value={5000}>5 secondes</MenuItem>
+          <MenuItem value={300000}>5 minutes</MenuItem>
+          <MenuItem value={900000}>15 minutes</MenuItem>
+          <MenuItem value={1500000}>25 minutes</MenuItem>
+          <MenuItem value={3600000}>1 hours</MenuItem>
+        </Select>
       </FormControl>
     </div>
   );
